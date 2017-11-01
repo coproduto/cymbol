c327bf7efc2e48edd4d4ba36b921b6d0158994f5
Pedro Castilho <cstl.px@gmail.com>
2017-10-31 23:58:42 -0300

    Added call graph abstraction

--- Cymbol/src/graph/CallGraph.java
+++ Cymbol/src/graph/CallGraph.java
@@ -1,5 +1,51 @@
 package graph;
 
-public class CallGraph {
+import org.antlr.v4.runtime.Token;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class CallGraph {
+	private List<String> funcNames = new ArrayList<String>();
+	private Map<String, List<String>> funcCalls = new HashMap<String, List<String>>();
+	
+	public List<String> getDeclaredFunctions() {
+		return funcNames;
+	}
+	
+	public Map<String, List<String>> getFunctionCalls() {
+		return funcCalls;
+	}
+	
+	public boolean declareToken(Token funcDecl) {
+		String name = funcDecl.getText();
+		if(!funcNames.contains(name)) {
+			funcNames.add(name);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void assertFunctionCall(Token caller, Token callee) {
+		String callerName = caller.getText();
+		String calleeName = callee.getText();
+		
+		List<String> callees;
+		if(funcCalls.containsKey(callerName)) {
+			callees = new ArrayList<String>();
+		} else {
+			callees = funcCalls.get(callerName);
+		}
+		
+		if(!callees.contains(calleeName)) {
+			callees.add(calleeName);
+		} else {
+			System.out.println("Dropped repeated call from " + callerName + " to " + calleeName);
+		}
+		
+		funcCalls.put(callerName, callees);
+	}
 }
